c11e9617a8ec8a717dda8d53a65432623c344db2
Rhythm Bhetwal <100419159+mythrhyth@users.noreply.github.com>
2022-11-20 19:44:47 +0545

    CSS

--- script.js
+++
@@ -1,8 +0,0 @@
-//responsive navigation bar
-let menu = document.querySelector('#check');
-let navbar = document.querySelector('.navbar'); 
-
-menu.onclick =() => {
-    menu.classList.toggle('fas fa-bars'); 
-    navbar.classList.toggle('open');
-}
